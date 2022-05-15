d731a12f1ac4970ad78e2f0cb99dd9793c113d5b
Alexander Shearer <alexshearer00@gmail.com>
2022-05-15 10:02:31 +0000

    Delete unused algo

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ This file represents the main entry point to the CamoWorms program.
 import sys
 import cProfile
 import pstats
-import imageio
 from src.image_loading import load_image
 from src.image_manipulation import crop
-from src.implementations.basic_genetic import run_basic_genetic
 from src.implementations.greedy import run_greedy
 from src.implementations.pso_quadratic import run_quadratic_search_pos
 from src.implementations.pso_worm import run_worm_search_pos
@@ -19,8 +17,6 @@ def main() -> None:
     image = crop(load_image("images", "original"), (320, 560, 160, 880))
 
     with cProfile.Profile() as pr:
-        # run_basic_genetic(image, clew_size=10,
-        #                   number_of_clews=100, total_iterations=400)
         # run_greedy(image, clew_size=10, total_generations=total_generations)
         # run_quadratic_search_pos()
         run_worm_search_pos(image, 200)
